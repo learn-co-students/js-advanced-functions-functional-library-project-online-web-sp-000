11460f525c86c2ff9b899261c8a1208ca337168a
Yingqi Chen <chanwk18@hotmail.com>
2020-01-07 05:49:42 +0000

    Done.

--- index.js
+++ index.js
@@ -5,22 +5,75 @@ const fi = (function() {
     },
 
     each: function(collection,callback) {
-         for const c of collection {
-         c.callback()
+      if (Array.isArray(collection)){
+      for (const c of collection) {
+         callback.call(this,collection)
        }
+     }else{
+       for (const c in collection) {
+        callback.call(this,collection[c])
+       }
+     }
+     return collection
     },
 
-    map: function() {
-
+    map: function(collection,callback) {
+      if (Array.isArray(collection)){
+        return collection.map(callback)
+      }else{
+        return Object.values(collection).map(callback)
+      }
     },
 
-    reduce: function() {
-
+    reduce: function(collection,callback,acc) {
+      return acc? collection.reduce(callback,acc) : collection.reduce(callback)
     },
 
-    functions: function() {
-
+    find: function(collection,condition) {
+       return collection.find(condition)
     },
+
+    filter: function(collection,condition){
+      return collection.filter(condition)
+    },
+
+    size: function(collection){
+      return Array.isArray(collection)? collection.length : Object.keys(collection).length
+    },
+
+    first: function(array,n){
+      return !n? array[0] : array.slice(0,n)
+    },
+
+    last: function(array,n){
+      return n? array.slice(-n) : array[array.length-1]
+    },
+
+    compact: function(collection){
+      return collection.filter(x=>!!x)
+    },
+
+    sortBy: function(collection,callback){
+      let copyOfCollection = collection.slice()
+      return copyOfCollection.sort(function(a,b){return callback(a)-callback(b)})
+    },
+
+    flatten: function(array,name=false){
+      if (!name){
+        while (array.find(e=>Array.isArray(e))){
+          array = array.flat()
+        }
+      }else{
+          array = array.flat()
+        }
+       return array
+      }
+
+
+
+
+
+
 
 
   }
