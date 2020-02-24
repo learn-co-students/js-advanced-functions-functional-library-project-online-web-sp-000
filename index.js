33ea94c1d28b4ff09444384a9f0675ae9f5e73b6
Jesse Barnes <jessekathrynbarnes@gmail.com>
2020-02-23 22:41:02 -0500

    Done.

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, e) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+      for (let i = 0; i < newCollection.length; i++)
+        e(newCollection[i])
 
+        return collection
     },
 
-    map: function() {
+    map: function(collection, iteratee) {
+      if (!(collection instanceof Array))
+        collection = Object.values(collection)
 
+      const newArr = []
+
+      for (let idx = 0; idx < collection.length; idx++)
+        newArr.push(iteratee(collection[idx]))
+
+      return newArr
     },
 
     reduce: function() {
-
+     
     },
 
     functions: function() {
