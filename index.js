d2a3308e72991b2e57a05605c114071f532966c3
Paul Rosero <paul_rosero@yahoo.com>
2020-03-01 11:10:16 -0500

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      let answer = fi.each(element, index, collection)
+      console.log(callback)
+      return answer
     },
 
     map: function() {
