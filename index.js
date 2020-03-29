af025997be92f470198892b178be8e56e3801bc2
BlythePollard <github email address>
2020-03-29 11:39:51 -0600

    Done.

--- index.js
+++ index.js
@@ -20,18 +20,68 @@ const fi = (function() {
     },
 
     map: function(collection, callback) {
-      return collection.map(x => callback(x))
+      if(typeof collection === 'object') {
+        const newCollection = Object.values(collection)
+        return newCollection.map(x => callback(x))
+      } else {
+        return collection.map(x => callback(x))
+      }
+     
       //produces new array by mapping each value in collection through callback
       //returns new collection without modifying original
     },
 
-    reduce: function() {
+    reduce: function(collection, callback, accumulator) {
+      let r = (!!accumulator) ? accumulator : collection[0]
+      let i = (!!accumulator) ? 0 : 1
+      for (; i < collection.length; i++) {
+        r = callback(r, collection[i])
+      }
+      return r
+      //with reduce, would be collection.reduce(callback, accumulator)
+    },
+
+    find: function(collection, predicate) {
+      if(typeof collection === 'object') 
+        collection = Object.values(collection)
+
+        for (let i = 0; i < collection.length; i++)
+          if(predicate(collection[i])) return collection[i]
+      
+          return undefined
+    },
+
+    filter: function(collection, predicate) {
+      if(typeof collection === 'object')
+      collection = Object.values(collection)
+      const newValues = []
+      for (let i = 0; i < collection.length; i++) 
+        if(predicate(collection[i])) 
+        newValues.push(collection[i])
+      return newValues  
+    },
+
+    size: function() {
 
     },
 
-    functions: function() {
+    first: function() {
 
     },
+
+    last: function() {
+
+    },
+
+    compact: function() {
+
+    }, 
+
+    sortBy: function() {
+
+    },
+
+
 
 
   }
