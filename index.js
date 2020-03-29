dc2f0dcf416c6a694495e1352b2e21251b3458f4
BlythePollard <github email address>
2020-03-28 18:26:39 -0600

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, alert) {
+      if(typeof collection === 'object') {
+        const newCollection = Object.values(collection)
+        for (let i = 0; i < newCollection.length; i++) {
+          alert(newCollection[i])
+        }
+       } else {
+          const newCollection = collection
+          for (let i = 0; i < newCollection.length; i++) {
+            alert(newCollection[i])
+          }
+        }
+      return collection
     },
 
     map: function() {
