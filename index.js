81054fa4228824f26540d61d37932346343093bc
Lindsey Wells <github email address>
2020-04-06 09:57:28 +0100

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 const fi = (function() {
-  return {
-    libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
-    },
+    return {
+        libraryMethod: function() {
+            return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
+        },
 
-    each: function() {
+        each: function(collection, callback) {
+            const collectionCopy = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
-    },
+            for (let i = 0; i < collectionCopy.length; i++)
+                callback(collectionCopy[i])
+            return collection
+        },
 
-    map: function() {
-
-    },
-
-    reduce: function() {
-
-    },
-
-    functions: function() {
-
-    },
+        map: function(collection, callback) {
+            const collectionCopy = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+            let newArray = [];
+            for (let i = 0; i < collectionCopy.length; i++)
+                newArray.push(callback(collectionCopy[i]))
+            return newArray
+        },
 
 
-  }
+        reduce: function() {},
+
+        functions: function() {
+
+        },
+
+
+    }
 })()
 
 fi.libraryMethod()
