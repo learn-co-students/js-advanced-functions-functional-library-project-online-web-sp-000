38aeaec0ab495d1bc19b67b702be8ac7c8aa721c
Roberto-Livi <robertomlivi@outlook.com>
2020-07-17 21:56:01 -0400

    Done.

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(obj, callback) {
+      Object.values(obj).forEach(x => callback(x))
+      return obj
     },
 
-    map: function() {
-
+    map: function(obj, callback) {
+      let newArray = []
+      Object.values(obj).map(x => newArray.push(callback(x)))
+      return newArray
     },
 
-    reduce: function() {
+    reduce: function(collection = [], callback = () => {}, acc) { s
+      let accumulator = acc
+      let coll = collection
 
+      if(!acc){
+        accumulator = collection[0]
+        coll = collection.splice(1)
+      }
+
+      coll.forEach(element => accumulator = callback(accumulator, element, coll))
+      return accumulator
     },
 
     functions: function() {
@@ -25,3 +37,6 @@ const fi = (function() {
 })()
 
 fi.libraryMethod()
+
+
+
