31ca6c6f3adf1f2781a4ce16533c91dbd37addc9
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-15 09:44:59 -0400

    Done.

--- index.js
+++ index.js
@@ -4,16 +4,29 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      for (const key in collection) {
+        callback(collection[key], key, collection);
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      const newArray = [];
+      for (const key in collection) {
+        newArray.push(callback(collection[key], key, collection));
+      }
+      return newArray;
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc) {
+      //if no acc is supplied,
+      //first run through the loop is just added to the acc with no pass through the function
+      // let memo = collection[0];
+      // for (let i = 1, i < collection.length, i++) {
+      //   memo = callback(memo, element, collection);
+      // }
+      // return memo;
     },
 
     functions: function() {
