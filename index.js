50457c45dd98206a358631c3f9cf596f6280442c
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-17 09:24:54 -0400

    Done.

--- index.js
+++ index.js
@@ -22,11 +22,111 @@ const fi = (function() {
     reduce: function(collection, callback, acc) {
       //if no acc is supplied,
       //first run through the loop is just added to the acc with no pass through the function
-      // let memo = collection[0];
-      // for (let i = 1, i < collection.length, i++) {
-      //   memo = callback(memo, element, collection);
-      // }
-      // return memo;
+      if (acc) {
+       let memo = acc;
+       for (let i = 0; i < collection.length; i++) {
+         memo = callback(memo, collection[i], collection);
+       }
+       return memo;
+      }
+      else {
+        let memo = collection[0];
+        for (let i = 1; i < collection.length; i++) {
+          memo = callback(memo, collection[i], collection);
+        } 
+        return memo;       
+      }
+    },
+
+    find: function(collection, predicate) {
+      for (const element of collection) {
+        if (predicate(element)) {
+          return element;
+        }
+      }
+    },
+
+    filter: function(collection, predicate) {
+      const newArray = []
+      for (const element of collection) {
+        if (predicate(element)) {
+          newArray.push(element);
+        }
+      }
+      return newArray;
+    },
+
+    size: function(collection) {
+      let i = 0;
+      for (const key in collection) {
+        i++
+      }
+      return i;
+    },
+
+    first: function(array, n=1) {
+      const newArray = []
+      for (let i = 0; i < n; i++) {
+        newArray.push(array[i])
+      } 
+      if (n > 1) {
+        return newArray; 
+      }
+      else {
+        return newArray[0];
+      }
+    },
+
+    last: function(array, n=1) {
+      const lastIndex = array.length - 1;
+      const count = lastIndex - n
+      const newArray = []
+      for (let i = lastIndex; i > count; i--) {
+        newArray.unshift(array[i])
+      } 
+      if (n > 1) {
+        return newArray; 
+      }
+      else {
+        return array[lastIndex];
+      }
+    }, 
+
+    compact: function(array) {
+      const newArray = []
+      for (const element of array) {
+        if (element) {
+          newArray.push(element);
+        }
+      }
+      return newArray;
+    },  
+
+
+   sortBy: function(array, callback) {
+     const arrayOfObjects = [];
+     const arrayOfKeys = [];
+     for (const element of array) {
+       const returnValue = callback(element);
+       const newObject = {};
+       newObject[returnValue] = element;
+       arrayOfObjects.push(newObject);
+       arrayOfKeys.push(returnValue);
+     }
+      const sortedArray = arrayOfKeys.sort(function(a, b) {
+      return a - b;
+      });
+
+     const finalArray = [];
+     for (const element of sortedArray) {
+        for (const nestedElement of arrayOfObjects) {
+          if (Object.keys(nestedElement)[0] === element.toString()) {
+            finalArray.push(Object.values(nestedElement)[0]);
+            break
+          }
+        }
+     }
+     return finalArray;
     },
 
     functions: function() {
