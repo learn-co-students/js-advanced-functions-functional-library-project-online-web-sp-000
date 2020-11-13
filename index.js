b7126bba39b5543e7926ee6974561bad70a6ef64
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-13 07:25:52 +0000

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(items, callback) {
+      if (items.constructor() === []) {
+        const arr = items;
+      } else if (items.constructor() === {}) {
+        const arr = Object.values(items);
+      }
+      arr.forEach(function(e) {
+        callback(e)
+      });
+      return items;
     },
 
     map: function() {
