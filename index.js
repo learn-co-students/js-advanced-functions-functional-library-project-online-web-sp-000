d06fea15ba7794616f99b182beb33f724383b4a4
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-28 17:08:56 +0000

    Done.

--- index.js
+++ index.js
@@ -6,22 +6,69 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+//instanceOf checks to see if the instance is in the collection
+//?:
+//if the instance is in the array then takes out the value using slice
+//If not then return collection of values
+each: function(collection, iteratee) {
+   const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
-    },
+   for (let idx = 0; idx < newCollection.length; idx++)
+     iteratee(newCollection[idx])
 
-    map: function() {
+   return collection
+ },
 
-    },
+ map: function(collection, iteratee) {
+   if (!(collection instanceof Array))
+     collection = Object.values(collection)
 
-    reduce: function() {
+   const newArr = []
 
-    },
+   for (let idx = 0; idx < collection.length; idx++)
+     newArr.push(iteratee(collection[idx]))
 
-    functions: function() {
+   return newArr
+ },
 
-    },
 
+ reduce: function(c = [], callback = () => {}, acc) {
+   let collection = c.slice(0)
+
+   if (!acc) {
+     acc = collection[0]
+     collection = collection.slice(1)
+   }
+
+   let len = collection.length;
+
+   for (let i = 0; i < len; i++) {
+     acc = callback(acc, collection[i], collection)
+   }
+   return acc;
+ },
+
+ find: function(collection, predicate) {
+   if (!(collection instanceof Array))
+     collection = Object.values(collection)
+
+   for (let idx = 0; idx < collection.length; idx++)
+     if (predicate(collection[idx])) return collection[idx]
+
+   return undefined
+ },
+
+ filter: function(collection, predicate){
+    if (!(collection instanceof Array))
+    collection = Object.values(collection)
+
+    let newArr = []
+
+    for (let i = 0; i < collection.length; i++)
+    if (predicate(collection[i])) newArr.push(collection[i])
+
+    return newArr
+ }
 
   }
 })()
