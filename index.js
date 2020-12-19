ebc3cbf6c2bcdf876f8bd2ea35eab0468ee13634
denalibalser <denali.balser167@gmail.com>
2020-12-18 17:26:21 -0700

    Done.

--- index.js
+++ index.js
@@ -4,12 +4,34 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      let newArray
+      if(typeof collection === 'object') {
+       newArray = Object.values(collection)
+      }
+      else {
+        newArray = collection
+      }
+      for (let i = 0; i < newArray.length; i++) {
+        callback(newArray[i], i, newArray)
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newArray
+      //const newCollection = new Array()
+      if(typeof collection === 'object') {
+       newArray = Object.values(collection)
+      }
+      else {
+        newArray = collection
+      }
+      for(let i = 0; newArray.length; i++) {
+        callback(newArray[i], i, newArray)
+         
+      }
+      return newArray
     },
 
     reduce: function() {
