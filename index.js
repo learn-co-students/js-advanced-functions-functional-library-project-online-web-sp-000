9e727a9159d11980752a07ceedf6b13278b298dd
CoderJay06 <jaycru06@yahoo.com>
2021-01-10 01:25:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,59 @@
 const fi = (function() {
-  return {
-    libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
-    },
+    return {
+        libraryMethod: function() {
+            return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
+        },
 
-    each: function() {
+        each: function(collection, callback) {
+            for (const key in collection) {
+                callback(collection[key], key, collection)
+            }
 
-    },
+            return collection
+        },
 
-    map: function() {
+        map: function(collection, callback) {
+            let newArray = []
+            for (const key in collection) {
+                newArray.push(callback(collection[key], key, collection))
+            }
+            return newArray
+        },
 
-    },
+        reduce: function(collection, callback, acc = 0) {
+            // Reduce boils down a collection of values into a single value. 
+            // Acc (short for accumulator) starts as the initial state of 
+            // the reduction, and with each successive step it should be 
+            // accumulate the return value of callback. The callback is passed 
+            // three arguments: the acc, the current value in our iteration 
+            // (the element in the array), and finally a reference to the entire 
+            // collection.
+            let returnVal = 0
+            collection.forEach(element => {
+                returnVal += callback(0, element, collection)
+            })
+            returnVal += acc
+            return returnVal
+        },
 
-    reduce: function() {
+        find: function(collection, predicate) {
+            let foundElement, element
+            for (let i = 0; i < collection.length; i++) {
+                foundElement = predicate(collection[i])
+                if (foundElement) {
+                    element = collection[i]
+                    break
+                }
+            }
+            return element
+        },
 
-    },
+        functions: function() {
 
-    functions: function() {
-
-    },
+        },
 
 
-  }
+    }
 })()
 
 fi.libraryMethod()
