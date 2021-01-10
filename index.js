e3fccf84477ce63e3c593556573c16889288ac26
Jason J Cruz <jaycru06@yahoo.com>
2021-01-10 05:13:11 +0000

    Done.

--- index.js
+++ index.js
@@ -12,12 +12,28 @@ const fi = (function() {
       return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newArray = []
+      for (const key in collection) {
+        newArray.push(callback(collection[key], key, collection))
+      }
+      return newArray
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc = 0) {
+      // Reduce boils down a collection of values into a single value. 
+      // Acc (short for accumulator) starts as the initial state of 
+      // the reduction, and with each successive step it should be 
+      // accumulate the return value of callback. The callback is passed 
+      // three arguments: the acc, the current value in our iteration 
+      // (the element in the array), and finally a reference to the entire 
+      // collection.
+      let returnVal = 0
+      collection.forEach(element => {
+        returnVal += callback(0, element, collection)
+      })
+      returnVal += acc
+      return returnVal
     },
 
     functions: function() {
