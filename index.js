46b40519864ca1c1b69918825f36502995a2526c
Yumacorn <ReachRyanMui@gmail.com>
2021-01-11 21:45:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,121 @@
+    let testObj1 = [1, 2, 3, 4];
+    let callback1 = x => (x * 3);
+
+    const testObj2 = {one: 1, two: 2, three: 3, four: 4}
+    const resultObj2 = {one: 3, two: 6, three: 9, four: 12}
+
+    // const arrResult1 = fi.map(testObj1, callback1);
+    // let m = arraysEqual([3, 6, 9, 12], arrResult1);
+
+    const testArr = testObj1.slice() // arr is [1, 2, 3, 4]
+    const callback = (acc, val, collection) => (acc + (val * 3))
+
+    function arraysEqual(arrA, arrB) {
+      if (arrA.length !== arrB.length) return false
+      for (let idx = 0; idx < arrA.length; idx++) {
+        if (arrA[idx] !== arrB[idx]) {
+          
+          if (isNaN(arrA[idx]) && isNaN(arrB[idx])) continue
+          return false
+        }
+      }
+      // console.log(arrA);
+      // console.log(arrB);
+      return true
+    }
+
 const fi = (function() {
   return {
     libraryMethod: function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
-
-    each: function() {
-
+    each: function(collection, callback) {
+      if (collection instanceof Array) {
+        let x = Object.assign([], collection);
+        for (let i = 0, len = x.length; i < len; i++) {
+          callback(x[i]);
+        }
+      } else {
+        let x = Object.values(collection);
+        for (let i = 0, len = x.length; i < len; i++) {
+          callback(x[i]);
+        }
+      } 
+      return collection;
     },
 
-    map: function() {
-
+// fi.map(testObj1, callback1)
+    map: function(obj, func) {    
+      if (Array.isArray(obj)) {
+        let x = Object.assign([], obj);
+        let result = Object.assign([]);
+        for (let i = 0, len = x.length; i < len; i++) {
+          result.push(func(x[i]));
+        }
+        return result;
+      } else {
+        let x = Object.assign({},obj);
+        let result = Object.assign({});
+        for (const property in x) {
+          // console.log(x[property]);
+          result[property] = func(x[property]);
+          // console.log(Object.values(result));
+        }
+        return Object.values(result);
+      }
     },
 
-    reduce: function() {
-
+    reduce: function(obj, func, acc = 0) {
+      if (acc != 0) {
+        let x = Object.assign([], obj);
+        for (let i = 0, len = x.length; i < len; i++) {
+          acc = func(acc, x[i]);
+        }
+        return acc;
+      } else {
+        let x = Object.assign([], obj);
+        acc = obj[0];
+        for (let i = 1, len = x.length; i < len; i++) {
+          acc = func(acc, x[i]);
+        }
+        return acc;
+      }
     },
 
-    functions: function() {
+    find: function(collection, predicate) {
+
+    },
+    filter: function(collection, predicate) {
+
+    },
+    size: function(collection) {
+
+    },
+    first: function(array, [n]) {
+
+    },
+    last: function(array, [n]) {
+
+    },
+    compact: function(array) {
+
+    },
+    sortBy: function(array, callback) {
+
+    },
+    flatten: function(array, [shallow]) {
+
+    },
+    uniq: function(array, [isSorted], [callback]) {
+
+    },
+    keys: function(object) {
+
+    },
+    values: function(object) {
+
+    },
+    functions: function(object) {
 
     },
 
@@ -25,3 +124,6 @@ const fi = (function() {
 })()
 
 fi.libraryMethod()
+
+const objResult = fi.map(testObj2, callback1)
+let m = arraysEqual([3, 6, 9, 12], objResult)
