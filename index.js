05ecd77c05b10357cd84498caa889f79e1efe860
Catherine Gloria <catherinegloria@gmail.com>
2021-02-14 10:49:44 -0800

    each and map

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+      const array = (collection instanceof Array) ? collection : Object.values(collection)
 
+      for (let i = 0; i < array.length; i++) {
+      callback(array[i])
+      }
+      return collection
     },
 
-    map: function() {
+    map: function(collection, callback) {
+      const array = (collection instanceof Array) ? collection : Object.values(collection)
 
+      let newArr = [];
+      for (let i = 0; i < array.length; i++) {
+      newArr.push(callback(array[i]))
+      }
+      return newArr
     },
 
     reduce: function() {
