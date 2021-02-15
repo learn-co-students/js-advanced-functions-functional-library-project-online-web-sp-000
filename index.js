5daa8b1e512322e08663b51944d96f43bc5974f2
Catherine Gloria <catherinegloria@gmail.com>
2021-02-15 06:32:38 -0800

    first and last

--- index.js
+++ index.js
@@ -63,8 +63,33 @@ const fi = (function() {
       return array
     },
 
+    size: function(collection) {
+      let count = 0;
 
+      if (!(collection instanceof Array)){
+        collection = Object.values(collection)
+      }
 
+      for (let i = 0; i < collection.length; i++) { count++ }
+
+      return count
+    },
+
+    first: function(array, n){
+      if (n) {
+        return array.slice(0, n)
+      } else {
+        return array[0]
+      }
+    },
+
+    last: function(array, n) {
+      if (n) {
+        return array.slice(-n, array[array.length - 1])
+      } else {
+        return array[array.length - 1]
+      }
+    },
 
   }
 })()
