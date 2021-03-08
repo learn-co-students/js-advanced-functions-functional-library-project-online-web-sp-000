1d7d1c10ca88a75a9c3b30ba2579d21aaaedcb1f
Chandra Lama <iamme24cl@gmail.com>
2021-03-07 20:35:11 -0800

    Done.

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+      const newCollection = (collection instanceof Array) ? collection : Object.values(collection);
 
+      for (let i = 0; i < newCollection.length; i++) {
+        callback(newCollection[i]);
+
+      }
+      return collection;
     },
 
-    map: function() {
+    map: function(collection, callback) {
+      if (!(collection instanceof Array)) {
+        collection = Object.values(collection); 
+      } 
+      const newCollection = [];
 
+      for (let i = 0; i < collection.length; i++) {
+        newCollection.push(callback(collection[i]));
+      }
+      return newCollection;
     },
 
     reduce: function() {
@@ -22,6 +36,6 @@ const fi = (function() {
 
 
   }
-})()
+})();
 
 fi.libraryMethod()
