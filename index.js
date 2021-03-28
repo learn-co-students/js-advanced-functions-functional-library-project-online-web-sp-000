0ba190f1922b983e4fb79c817c890f4e113e493f
RSigmaSG <sg.comparch@gmail.com>
2021-03-28 12:34:57 -0400

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(input, func) {
+      //console.log(input)
+      let array;
+      if (Array.isArray(input)) {
+        array = input;
+      }
+      else {
+        //console.log(input)
+        //console.log(Object.values(input))
+        array = Object.values(input);
+      }
+      console.log(array)
+      for (let i = 0; i < array.length; i++) {
+        array[i] = func(i);
+      }
+      return input;
     },
 
     map: function() {
