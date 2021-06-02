5467ec2999b8093fd63f6c71b0ca722b4698caef
kkirby16 <kkirby16@my.whitworth.edu>
2021-06-01 19:45:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fi = (function() {
   return {
+
+ 
     libraryMethod: function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
@@ -12,20 +14,116 @@ const fi = (function() {
       return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection) 
+      let newArray = []
+      for (let i = 0; i < newCollection.length; i++) { 
+        newArray.push(callback(newCollection[i]))
+      }
+return newArray
     },
 
-    reduce: function() {
+    reduce: function(collection, callback, acc) {
+  if (!acc) { 
+    let startingValue = collection[0] 
+    
+    for (let i = 1; i < collection.length; i++) { 
+      startingValue = callback(startingValue, collection[i])
+      //startingValue = startingValue + element * operation
+    }
+    return startingValue
+  }
+  else {
+      for (let i = 0; i < collection.length; i++) { 
+        acc = callback(acc, collection[i])
+      }
+      return acc
+    }},
 
+    find: function(collection, predicate) {
+      for (let i = 0; i < collection.length; i++) { 
+        if (predicate(collection[i]) == true) { 
+        return collection[i]}
+      }
     },
 
-    functions: function() {
-
+    filter: function(collection, predicate) { 
+    let filteredResults = []
+      for (let i = 0; i < collection.length; i++) { 
+        if (predicate(collection[i]) == true) { 
+          filteredResults.push(collection[i])
+        }
+      }
+      return filteredResults
     },
 
+    size: function(collection) { 
+const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+let size = 0 
+for (let i = 0; i < newCollection.length; i++) { 
+  size = i + 1
+}
+  return size
+},
 
+first: function(array, n) { 
+  if (n) { 
+  const changedArray = array.slice(0, n);
+  return changedArray
+  } 
+  else {
+  return array[0]
+  }
+},
+
+last: function(array, n) {
+if (n) { 
+  const changedArray = array.slice(-n);
+  return changedArray
+  } 
+  else {
+  return array[array.length - 1]
+  }
+},
+
+compact: function(array) { 
+  let temporaryArray = []
+  for (let i = 0; i < array.length; i++) { 
+    if (Boolean(array[i]) == true) { 
+        temporaryArray.push(array[i])
+    }
+  }
+return temporaryArray
+
+},
+
+sortBy: function(array, callback) { 
+  //if (typeof array[0] = "number") { }
+  let newArray = [...array]
+  return newArray.sort((a,b) => {return callback(a) - callback(b)})
+},
+
+uniq: function(array, isSorted, callback) { 
+  console.log(callback)
+  if (isSorted === false) { 
+    const sortedArray = fi.sortBy(array, callback)
+    const hash = {}, result = [];
+
+  for (var i = 0; i < sortedArray.length; i++)
+    for (var j = i + 1; j < arr.length; j++)
+        if (arr[i] === arr[j])
+        result.push(arr[i])
+
+  }
+  return array
+
+}
   }
 })()
 
-fi.libraryMethod()
+//fi.libraryMethod()
+
+
+// })()
+
+// fi.libraryMethod()
